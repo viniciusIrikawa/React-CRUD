e22fcb579af3ced906aab21a9dbdb4b5bd712833
Vinicius <viniciusiandrade@hotmail.com>
2022-05-16 00:09:55 -0300

    Bootswatch and Navbar

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      hiii
+    <div>
+      <Navbar></Navbar>
+      <div className="alert alert-dismissible alert-success">
+      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+      <strong> Well done! </strong>
+    </div>
     </div>
   );
 }
