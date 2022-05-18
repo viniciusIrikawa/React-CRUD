bc6351d918270ea906a304872f2d1e86b0cda387
Vinicius <viniciusiandrade@hotmail.com>
2022-05-17 23:53:38 -0300

    React Router and components

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,14 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './Components/Navbar';
+import { Home } from './Views/Home';
+import { Register } from './Views/Products/Register';
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-      <div className="alert alert-dismissible alert-success">
-      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-      <strong> Well done! </strong>
-    </div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/register' element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- crud/src/Components/Navbar.js
+++ crud/src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <Link className="navbar-brand" to='/'>Navbar</Link>
                     <button className="navbar-toggler" 
                             type="button" 
                             data-bs-toggle="collapse" 
@@ -19,15 +20,19 @@ export const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarColor01">
                         <ul className="navbar-nav me-auto">
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Home
+                                <Link to='/'className="nav-link"> Home 
                                     <span className="visually-hidden">(current)</span>
-                                </a>
+                                </Link>
+                                {/* <a className="nav-link" href="#">Home
+                                    <span className="visually-hidden">(current)</span>
+                                </a> */}
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Register</a>
+                                <Link to='/register' className="nav-link"> Register </Link>
+                                {/* <a className="nav-link" href="#"> Register </a> */}
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Appointments</a>
+                                <a className="nav-link" href="#"> Appointments </a>
                             </li>
                         </ul>
                     </div>
