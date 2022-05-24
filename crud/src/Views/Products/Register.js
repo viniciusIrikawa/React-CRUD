e12e1e4809b4f761c6ed86a5a9d558fc6a6496a1
viniciusIrikawa <viniciusiandrade@hotmail.com>
2022-05-24 16:23:44 -0300

    Saving form data in state

--- crud/src/Views/Products/Register.js
+++ crud/src/Views/Products/Register.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Register.css'
 
 export const Register = () => {
+
+  const initialData = {
+    Name: '', 
+    SKU: '', 
+    Description: '', 
+    Price: 0, 
+    Provider: ''
+  };
+
+  const [form, setForm] = useState(initialData);
+
+  const getData = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setForm({...form ,[name]:value })
+  }
+
+  const submitForm = () => {
+    console.log(form)
+  }
+  
+  const clearForm = () => {
+    setForm(initialData)
+    console.log(form)
+  }
+
   return (
     <div className='card'>
       <div className='card-header'>
@@ -12,14 +39,14 @@ export const Register = () => {
             <div className='col-md-6'>
               <div className='form-group'>
                 <label> *Name: </label>
-                <input type='text' className='form-control' />
+                <input type='text' className='form-control'  onChange={getData} name='Name' value={form.Name}/>
               </div>
             </div>
 
             <div className='col-md-6'>
               <div className='form-group'>
                 <label> *SKU: </label>
-                <input type='text' className='form-control' />
+                <input type='text' className='form-control' onChange={getData} name='SKU' value={form.SKU}/>
               </div>
             </div>
           </div>
@@ -28,7 +55,7 @@ export const Register = () => {
             <div className='col-md-12'>
               <div className='form-group'>
                 <label> Description: </label>
-                <textarea className='form-control' />
+                <textarea className='form-control' onChange={getData} name='Description' value={form.Description}/>
               </div>
             </div>
           </div>
@@ -37,14 +64,14 @@ export const Register = () => {
             <div className='col-md-6'>
               <div className='form-group'>
                 <label> *Price: </label>
-                <input type='text' className='form-control' />
+                <input type='text' className='form-control' onChange={getData} name='Price' value={form.Price}/>
               </div>
             </div>
 
             <div className='col-md-6'>
               <div className='form-group'>
                 <label> *Provider: </label>
-                <input type='text' className='form-control' />
+                <input type='text' className='form-control' onChange={getData} name='Provider' value={form.Provider}/>
               </div>
             </div>
           </div>
@@ -52,11 +79,11 @@ export const Register = () => {
           
           <div className='row'>
             <div className='col-md-1'>
-              <button className='btn btn-success'> Save </button>
+              <button className='btn btn-success' onClick={submitForm}> Save </button>
             </div>
 
             <div className='col-md-1'>
-              <button className='btn btn-primary'> Clear </button>
+              <button className='btn btn-primary' onClick={clearForm}> Clear </button>
             </div>
           </div>
 
