4e046b69afcc50563d91078267a76f1cb1453e4e
Vinicius <viniciusiandrade@hotmail.com>
2022-06-18 00:57:23 -0300

    Remove function

--- crud/src/Views/Products/Appointments.js
+++ crud/src/Views/Products/Appointments.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import './Appointments.css'
 
 export const Appointments = () => {
 
   const [products, setProducts] = useState([]);
 
+  const storedList = () => {
+    const storedList = JSON.parse(localStorage.getItem('_PRODUCTS'));
+    return storedList;
+  }
+
   useEffect(() => {
     if(localStorage.getItem('_PRODUCTS')){
-        const storedList = JSON.parse(localStorage.getItem('_PRODUCTS'));
-        setProducts(storedList);
-        console.log(products)
+        setProducts(storedList());
     }
   }, [])
+
+  const deleteData = (index) => {
+    const obj = storedList().filter((item, i) => i !== index)
+    localStorage.setItem('_PRODUCTS', JSON.stringify(obj))
+  }
 
   return (
       <div className='card'>
@@ -23,16 +32,21 @@ export const Appointments = () => {
                 <th scope="col"> SKU </th>
                 <th scope="col"> Price </th>
                 <th scope="col"> Provider </th>
+                <th scope="col"> Options </th>
               </tr>
             </thead>
             <tbody>
               {products.map((item, index) => {
                 return(
                   <tr key={index}>
-                    <th scope="row"> {item.Name} </th>
+                    <td scope="row"> {item.Name} </td>
                     <td> {item.SKU} </td>
                     <td> {item.Price} </td>
                     <td> {item.Provider} </td>
+                    <td> 
+                      <button className='btn btn-primary'> Edit </button> 
+                      <button className='btn btn-danger' onClick={() => deleteData(index)}> Delete </button>
+                    </td>
                   </tr>
                 )}
               )}
